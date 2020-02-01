9923a36bf5d3ca15f3cddc04b1fe84ea8e3f571f
Fábio Fernandes <fabio.frn81@gmail.com>
2020-02-01 10:30:38 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/rest/controller/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.rest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<String> hello(@PathVariable String nome) {
+        return ResponseEntity.ok(String.format("Ola %s", nome));
+    }
+}
