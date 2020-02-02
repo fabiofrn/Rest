db42adfd8182f67e60f6a6c3c5cd417cd31827d7
Fábio Fernandes <fabio.frn81@gmail.com>
2020-02-02 20:15:42 -0300

    Ajustes para deploy no tomcat.

--- src/main/java/br/com/rest/RestApplication.java
+++ src/main/java/br/com/rest/RestApplication.java
@@ -2,10 +2,16 @@ package br.com.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class RestApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
