062031479f51b17ed2c8e403c2605679adda0e48
Fábio Fernandes <fabio.frn81@gmail.com>
2020-02-02 07:36:44 -0300

    Adicionando plugin de versao do java.

--- src/main/java/br/com/rest/controller/HelloController.java
+++ src/main/java/br/com/rest/controller/HelloController.java
@@ -5,13 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 public class HelloController {
 
     @GetMapping("/{nome}")
     public ResponseEntity<String> hello(@PathVariable String nome) {
-        return ResponseEntity.ok(String.format("Ola %s", nome));
+        return ResponseEntity.ok(String.format("Ola %s como vai?", nome));
     }
 }
